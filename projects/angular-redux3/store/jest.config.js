f4c6293c72cddc7fe1af5463dfef4f8c57b680ce
pakozlow <55689646+pakozlow@users.noreply.github.com>
2026-02-20 09:37:32 +0100

    chore: update to angular 20 (#9)
    * chore: update to angular 20
    
    * cleanup
    
    ---------
    
    Co-authored-by: Patryk Kozłowski <patryk@local>

---
+++ projects/angular-redux3/store/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    roots: ['<rootDir>/projects/angular-redux3/store/src'],
+    testMatch: ['**/specs/**/*(*.)@(spec|test).ts?(x)'],
+    moduleNameMapper: {
+        '^@angular-redux3/store$': '<rootDir>/projects/angular-redux3/store/src/public-api.ts',
+        '^@angular-redux3/store/mocks$': '<rootDir>/projects/angular-redux3/store/mocks/public-api.ts'
+    },
+    modulePathIgnorePatterns: ['<rootDir>/dist'],
+    globals: {
+        'ts-jest': {
+            diagnostics: false
+        }
+    }
+};
